b9300c645bcdb8b8a7a276e84a345ffe4be03973
Lukam90 <lucien.hamm@gmail.com>
2023-03-02 21:46:54 +0100

    MAJ Ex Boot > API Read

--- README.md
+++ README.md
@@ -2,10 +2,60 @@
 
 ## ex-boot
 
-**Docker**
+### Docker
+
+**Download**
 
 docker compose up -d
 
+**Containers List**
+
 docker compose ps
 
+**PostgreSQL Logs**
+
 docker logs postgres -f
+
+**Bash Console**
+
+docker exec -it postgres bash
+
+### PostgreSQL
+
+**Connexion**
+
+psql -U lukas
+
+**List Databases**
+
+\l
+
+**Quit**
+
+\q
+
+exit
+
+**Connect to DB**
+
+\c customer
+
+**List Relations**
+
+\dt
+
+### SQL Commands
+
+**Create**
+
+create database customer;
+
+**Select**
+
+select * from customer;
+
+**Insert**
+
+insert into customer(id, name, email, age) values(nextval('customer_id_sequence'), 'Alex', 'alex@test.com', 22);
+
+insert into customer(id, name, email, age) values(nextval('customer_id_sequence'), 'Kid', 'kid@test.com', 33);
--- ex-boot/src/main/java/com/lukas/Main.java
+++ ex-boot/src/main/java/com/lukas/Main.java
@@ -5,30 +5,25 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+	private final CustomerRepository customerRepository;
+
+	public Main(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
-	@GetMapping("/")
-	public GreetResponse greet() {
-		return new GreetResponse(
-			"Hello!",
-			List.of("Java", "Golang", "Javascript"),
-			new Person("Alex")
-		);
+	@GetMapping
+	public List<Customer> getCustomers() {
+		return customerRepository.findAll();
 	}
-
-	record Person(String name) {}
-
-	record GreetResponse(
-		String greet,
-		List<String> favProgrammingLanguages,
-		Person person
-	){}
 }
